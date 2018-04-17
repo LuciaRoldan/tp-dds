5040e36a3a15eaae0065af6c2dba4bcecaaf74b2
morsamatias <matimorsa@est.frba.utn.edu.ar>
2018-04-16 23:44:01 -0300

    Agrego Json al POM

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,9 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Parser {
@@ -10,8 +13,31 @@ public class Parser {
 
         File archivo = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + nombreArchivo);
 
+
+        JSONArray a = (JSONArray) parser.parse(new FileReader(archivo));
+
+        for (Object o : a)
+        {
+            JSONObject person = (JSONObject) o;
+
+            String name = (String) person.get("name");
+            System.out.println(name);
+
+            String city = (String) person.get("city");
+            System.out.println(city);
+
+            String job = (String) person.get("job");
+            System.out.println(job);
+
+            JSONArray cars = (JSONArray) person.get("cars");
+
+            for (Object c : cars)
+            {
+                System.out.println(c+"");
+            }
+        }
+
         ObjectMapper mapper = new ObjectMapper();
-        String jsonInString = "{'name' : 'mkyong'}";
 
         //JSON from file to Object
         Dispositivo dispositivo = mapper.readValue(archivo, Dispositivo.class);
@@ -26,7 +52,6 @@ public class Parser {
             File archivo = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + nombreArchivo);
 
             ObjectMapper mapper = new ObjectMapper();
-            String jsonInString = "{'name' : 'mkyong'}";
 
             //JSON from file to Object
             Cliente cliente = mapper.readValue(archivo, Cliente.class);
@@ -35,3 +60,4 @@ public class Parser {
     }
 
 }
+
