ee760f26431732badcfa8f3553f03d09f72ba625
morsamatias <matimorsa@est.frba.utn.edu.ar>
2018-04-16 23:25:47 -0300

    Categoria con metodos cargo fijo, cargo variable y pertenece

--- src/main/java/CategoriaR2.java
+++ src/main/java/CategoriaR2.java
@@ -1,2 +1,14 @@
+
+
+// R2 150 < C <= 325 cargo fijo = 35,32  cargo variable = 0,644
+
+
 public class CategoriaR2 implements CategoriaResidencial{
+    public double cargoVariable() { return 0.644; }
+
+    @Override
+    public double cargoFijo() { return 35.32;}
+
+    @Override
+    public boolean pertenece(double consumo) { return ((150 < consumo) && (consumo <= 325)); }
 }
--- src/main/java/CategoriaR6.java
+++ src/main/java/CategoriaR6.java
@@ -1,2 +1,14 @@
+// R6 500<C<=600 CARGO FIJO =220,75 // CARGO VARIABLE = 0,832
+
+
 public class CategoriaR6 implements CategoriaResidencial{
+
+    @Override
+    public double cargoVariable() { return 0.832; }
+
+    @Override
+    public double cargoFijo() {return 220.75; }
+
+    @Override
+    public boolean pertenece(double consumo) { return  ((500 < consumo) && (consumo <= 600));}
 }
