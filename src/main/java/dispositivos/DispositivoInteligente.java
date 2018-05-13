7129bd59636e383b7637d621698e3faf481957a4
Unknown <lucia_roldan@outlook.com>
2018-05-13 11:56:25 -0300

    Agrego funcionalidades encendido/apagado

--- src/main/java/dispositivos/DispositivoInteligente.java
+++ src/main/java/dispositivos/DispositivoInteligente.java
@@ -2,6 +2,8 @@ package dispositivos;
 
 public class DispositivoInteligente extends TipoDeDispositivo {
     
+	private Estado estado;
+
 	DispositivoInteligente(){};
 	
 	@Override
@@ -14,4 +16,28 @@ public class DispositivoInteligente extends TipoDeDispositivo {
 	
 	@Override
     public String getNombre(){return "INTELIGENTE";}
+	
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	public boolean estaPrendido() {
+		return this.getEstado().estaPrendido();
+	}
+	
+	public boolean estaApagado() {
+		return this.getEstado().estaApagado();
+	}
+	
+	public void encendete() {
+		this.getEstado().encendete(this);
+	}
+	
+	public void apagate() {
+		this.getEstado().apagate(this);
+	}
 }
