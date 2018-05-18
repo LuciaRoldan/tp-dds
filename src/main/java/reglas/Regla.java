caf66d8c88ebf56d79f0e70fe1789927838b3c51
morsamatias <matimorsa@est.frba.utn.edu.ar>
2018-05-18 11:08:02 -0300

    Fix of sensors, actuators and rules

--- src/main/java/reglas/Regla.java
+++ src/main/java/reglas/Regla.java
@@ -3,15 +3,25 @@ package reglas;
 import java.util.ArrayList;
 import java.util.List;
 
-import dispositivos.Dispositivo;
+import actuadores.Actuador;
 
 public abstract class Regla {
-	List<Dispositivo> dispositivos = new ArrayList<>();
+	List<Actuador> actuadores = new ArrayList<Actuador>();
+
+	public static Float TEMPERATURAMINIMA = 12.5F;
+	public static Float INTENSIDADMAXIMA = 100F;
+	public static Boolean MOVIMIENTO = true;
 	
-	// ↓ Estos serian "actuadores" ↓
+	// ↓ Estos son las condiciones ↓
 	public void temperatura(float unaTemperatura) {};
 	public void movimientoAfuera(boolean hayMovimiento) {};
 	public void luz(float intensidad) {};
+
+	// Setters //
+	public void agregarActuador(Actuador actuador){this.actuadores.add(actuador);}
+	public void setTEMPERATURAMINIMA(Float TM){this.TEMPERATURAMINIMA = TM;}
+	public void setINTENSIDADMAXIMA(Float IM){this.INTENSIDADMAXIMA = IM;}
+	public void setMOVIMIENTO(Boolean mov){this.MOVIMIENTO = mov;}
 }
 
 /* ;;Ejemplo de como seria una Regla;;
@@ -19,8 +29,24 @@ public abstract class Regla {
 public class apagarLuz extends Regla{
 
 	@Override
-	public void luz(float intensidad){
-		this.dispositivos.forEach(dispositivo -> dispositivo.apagar() );
+	public void temperatura(Float unaTemperatura){
+		if (unaTemperatura < TEMPERATURAMINIMA)
+		this.actuadors.forEach(actuador -> actuador.haceFrio() );
 	}
+
+	@Override
+	public void movimientoAfuera(Boolean hayMovimiento){
+		if (hayMovimiento == MOVIMIENTO)
+		this.actuadores.forEach(actuador -> actuador.movimiento() );
+	}
+
+	@Override
+	public void movimientoAfuera(Boolean hayMovimiento){
+		if (hayMovimiento == MOVIMIENTO)
+		this.actuadores.forEach(actuador -> actuador.movimiento() );
+	}
+
+
+
 }
 */
--- src/main/java/sensores/Sensor.java
+++ src/main/java/sensores/Sensor.java
@@ -3,11 +3,13 @@ package sensores;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.org.apache.regexp.internal.RE;
 import reglas.Regla;
 
 
 public abstract class Sensor {
-	List<Regla> reglas = new ArrayList<>();
+
+	List<Regla> reglas = new ArrayList<Regla>();
 	
 	public void agregarRegla(Regla unaRegla){
 	      reglas.add(unaRegla);
