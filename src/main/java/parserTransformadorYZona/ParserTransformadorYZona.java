21e15a6d37713a2eaa1ae97cd85a8a5b43ced8e2
morsamatias <matimorsa@est.frba.utn.edu.ar>
2018-07-11 00:44:22 -0300

    Change function name in ParserTransformadosYZona

--- src/main/java/parserTransformadorYZona/ParserTransformadorYZona.java
+++ src/main/java/parserTransformadorYZona/ParserTransformadorYZona.java
@@ -14,40 +14,39 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 
 public class ParserTransformadorYZona {
 
-	JSONParser parser = new JSONParser();
-	ObjectMapper mapper = new ObjectMapper();
+    JSONParser parser = new JSONParser();
+    ObjectMapper mapper = new ObjectMapper();
 
 //	public Transformador parsearTransformador(String archivo) throws NoSePudoAbrirElArchivoException{
 //		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	
-	 public ParserTransformadorYZona(){
 
+    public ParserTransformadorYZona() {
 
-	 	mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	    }
-	 public Transformador parsearTransformador(String archivo) throws NoSePudoAbrirElArchivoException, IOException {
-		try {
-			Transformador transformador = mapper.readValue(new File(archivo), Transformador.class);
-			return transformador;
-		
-		} catch (IOException excepcion) {
-			//throw new NoSePudoAbrirElArchivoException("No se pudo abrir el archivo: "+ archivo);
-			throw excepcion;
-		}
-	}
-	
-	public Zona parsearZona(String archivo) throws NoSePudoAbrirElArchivoException{
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		
-		try {
-			Zona zona = mapper.readValue(new File(archivo), Zona.class);
-			return zona;
-		
-		} catch (IOException excepcion) {
-			throw new NoSePudoAbrirElArchivoException("No se pudo abrir el archivo: "+ archivo);
-		}
-	}
-	
-	
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public Transformador parsearTransformador(String archivo) throws NoSePudoAbrirElArchivoException, IOException {
+        try {
+            Transformador transformador = mapper.readValue(new File(archivo), Transformador.class);
+            return transformador;
+
+        } catch (IOException excepcion) {
+            //throw new NoSePudoAbrirElArchivoException("No se pudo abrir el archivo: "+ archivo);
+            throw excepcion;
+        }
+    }
+
+    public Zona parsearZona(String archivo) throws NoSePudoAbrirElArchivoException {
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        try {
+            Zona zona = mapper.readValue(new File(archivo), Zona.class);
+            return zona;
+
+        } catch (IOException excepcion) {
+            throw new NoSePudoAbrirElArchivoException("No se pudo abrir el archivo: " + archivo);
+        }
+    }
 }
+
 
