8e405a3a235cc0734196f2e33aec36607135c32b
Unknown <rociochipian@gmail.com>
2018-07-13 17:50:35 -0300

    test simplex

--- src/test/java/testsCliente/TestsSimplex.java
+++ src/test/java/testsCliente/TestsSimplex.java
@@ -2,8 +2,11 @@ package testsCliente;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import categoria.CategoriaResidencial;
@@ -21,58 +24,61 @@ import dispositivosConcretos.Plancha;
 import dispositivosConcretos.Televisor;
 import dispositivosConcretos.TipoPantalla;
 import dispositivosConcretos.Ventilador;
+import mock.EncendidoMock;
 import usuario.Cliente;
 import usuario.TipoDocumento;
 
 public class TestsSimplex {
 	
+			//-----ARIEL-----//
+	
+			//Dispositivos de Ariel//
+					
+			ArrayList<DispositivoConcreto> dispositivos = new ArrayList<DispositivoConcreto>();
+					
+			DispositivoInteligente tvBase = new DispositivoInteligente("tvBase", new Encendido(), 90, 360);
+			Televisor tv = new Televisor(tvBase, TipoPantalla.LED, 40);
+					
+			DispositivoInteligente lamparaBase = new DispositivoInteligente("lamparaBase", new Encendido(), 90, 360);
+			Lampara lampara = new Lampara(lamparaBase, false, 11);
+					
+			DispositivoEstandar lavarropasBase = new DispositivoEstandar("lavarropasBase", 6, 30);
+			Lavarropas lavarropas = new Lavarropas(lavarropasBase, false, false, 5);
+					
+			DispositivoInteligente pcBase = new DispositivoInteligente("pcBase", new Encendido(), 60, 360);
+			PC pc = new PC(pcBase, true);
+					
+			DispositivoInteligente aireBase = new DispositivoInteligente("aireBase", new Encendido(), 90, 360);
+			AireAcondicionado aireAcondicionado = new AireAcondicionado(aireBase, 2200);
+
+			DispositivoEstandar microondasBase = new DispositivoEstandar("microondasBase", 3, 15);
+			Microondas microondas = new Microondas(microondasBase, true);
+					
+			DispositivoEstandar planchaBase = new DispositivoEstandar("planchaBase", 30, 30);
+			Plancha plancha = new Plancha(planchaBase, true);
+					
+			DispositivoInteligente ventiladorBase = new DispositivoInteligente("ventiladorBase", new Encendido(), 120, 360);
+			Ventilador ventilador = new Ventilador(ventiladorBase, true);
+			
+	@Before		
+	public void antesQueNada() {
+				dispositivos.add(tv);
+				dispositivos.add(lampara);
+				dispositivos.add(lavarropas);
+				dispositivos.add(pc);
+				dispositivos.add(aireAcondicionado);
+				dispositivos.add(microondas);
+				dispositivos.add(plancha);
+				dispositivos.add(ventilador);
+	}
+			
+	
 	@Test
 	public void testSimplexAriel() {
-		
-		//-----ARIEL-----//
-		
-		//Dispositivos de Ariel//
-				
-		ArrayList<DispositivoConcreto> dispositivos = new ArrayList<DispositivoConcreto>();
-				
-		DispositivoInteligente tvBase = new DispositivoInteligente("tvBase", new Encendido(), 90, 360);
-		Televisor tv = new Televisor(tvBase, TipoPantalla.LED, 40);
-				
-		DispositivoInteligente lamparaBase = new DispositivoInteligente("lamparaBase", new Encendido(), 90, 360);
-		Lampara lampara = new Lampara(lamparaBase, false, 11);
-				
-		DispositivoEstandar lavarropasBase = new DispositivoEstandar("lavarropasBase", 6, 30);
-		Lavarropas lavarropas = new Lavarropas(lavarropasBase, false, false, 5);
-				
-		DispositivoInteligente pcBase = new DispositivoInteligente("pcBase", new Encendido(), 60, 360);
-		PC pc = new PC(pcBase, true);
-				
-		DispositivoInteligente aireBase = new DispositivoInteligente("aireBase", new Encendido(), 90, 360);
-		AireAcondicionado aireAcondicionado = new AireAcondicionado(aireBase, 2200);
-
-		DispositivoEstandar microondasBase = new DispositivoEstandar("microondasBase", 3, 15);
-		Microondas microondas = new Microondas(microondasBase, true);
-				
-		DispositivoEstandar planchaBase = new DispositivoEstandar("planchaBase", 30, 30);
-		Plancha plancha = new Plancha(planchaBase, true);
-				
-		DispositivoInteligente ventiladorBase = new DispositivoInteligente("ventiladorBase", new Encendido(), 120, 360);
-		Ventilador ventilador = new Ventilador(ventiladorBase, true);
-				
-			dispositivos.add(tv);
-			dispositivos.add(lampara);
-			dispositivos.add(lavarropas);
-			dispositivos.add(pc);
-			dispositivos.add(aireAcondicionado);
-			dispositivos.add(microondas);
-			dispositivos.add(plancha);
-			dispositivos.add(ventilador);
-			
+	
 		Cliente ariel = new Cliente("Ariel Boghossian", "Cabildo 1099", "01012000", "aboghoss", 
 				"hipopotamo", TipoDocumento.DNI, 18000999, 1500009999, CategoriaResidencial.CATEGORIAR3, dispositivos);
-		
 		ariel.configuracionOptima();
-		
 		
 		//Checkeado con http://www.phpsimplex.com/simplex
 		assertEquals((double) 360, tv.getConsumoIdeal(), 0);
@@ -84,4 +90,25 @@ public class TestsSimplex {
 		assertEquals((double) 30, plancha.getConsumoIdeal(), 0);
 		assertEquals((double) 360, ventilador.getConsumoIdeal(), 0);
 	}
+	
+	
+	@Test
+	public void testEjecutarSimplexAriel() {
+		
+		Cliente ariel = new Cliente("Ariel Boghossian", "Cabildo 1099", "01012000", "aboghoss", 
+				"hipopotamo", TipoDocumento.DNI, 18000999, 1500009999, CategoriaResidencial.CATEGORIAR3, dispositivos);
+		
+		ariel.ejecutarSimplex();
+		
+		LocalDateTime hoy = LocalDateTime.now();
+		LocalDateTime ayer = hoy.minus(1, ChronoUnit.DAYS);
+		
+		dispositivo.agregarEstado(new EncendidoMock(ayer, hoy));
+		
+		double consumo = dispositivo.calcularConsumoUltimasNHoras(24);
+	
+		// 2,000*24 = 48,000
+		assertEquals(0, consumo,0.05);
+	}
 }
+
