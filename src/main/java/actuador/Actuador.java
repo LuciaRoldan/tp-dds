2372bb8ccdaa7a456af68fcb35a5882eb54d0269
morsamatias <matimorsa@est.frba.utn.edu.ar>
2018-09-03 08:36:08 -0300

    Ordeno

--- src/main/java/actuador/Actuador.java
+++ src/main/java/actuador/Actuador.java
@@ -4,13 +4,13 @@ import dispositivo.DispositivoInteligenteInterfaz;
 
 
 public abstract class Actuador {
-	
+
     DispositivoInteligenteInterfaz dispositivo;
 
     public Actuador(DispositivoInteligenteInterfaz dispositivo) {
-    	this.dispositivo = dispositivo;
+        this.dispositivo = dispositivo;
     }
 
     public abstract void actuar();
-    
+
 }
