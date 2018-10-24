20775a5eb72cd3f528ae6504a15362966cec9de3
Unknown <rociochipian@gmail.com>
2018-10-24 00:10:36 -0300

    mere

--- src/main/java/contralador/ControladorLogin.java
+++ src/main/java/contralador/ControladorLogin.java
@@ -1,5 +1,17 @@
 package contralador;
 
-public class ControladorLogin {
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class ControladorLogin implements WithGlobalEntityManager, TransactionalOps{
+	
+	public ModelAndView mostrar(Request request, Response response) {
+		System.out.println("Mostrar Login");
+		return new ModelAndView(null, "login.hbs");
+	}
 
 }
