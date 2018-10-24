745cf2ae89f55be812b38f3358da1342336a8d7e
Ivana <ivi9609@gmail.com>
2018-10-24 20:01:06 -0300

    Crea el ControladorAdministrador

---
+++ src/main/java/controlador/ControladorAdministrador.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import java.io.IOException;
+
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class ControladorAdministrador implements WithGlobalEntityManager, TransactionalOps{
+
+	public ModelAndView mostrar(Request request, Response response) throws IOException {
+
+		return new ModelAndView(null , "admin.hbs");
+	}
+
+}
